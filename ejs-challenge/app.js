47e3fd5237702e89088170d1f40f9565dd24efed
frenchie43972 <frenchie43972@gmail.com>
2022-02-09 06:39:41 +0900

    Added content

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -15,12 +15,17 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.render("home", {homeStart: homeStartingContent});
+});
 
+app.get("/about", (req,res) => {
+  res.render("about", {aboutContent: aboutContent});
+});
 
-
-
-
-
+app.get("/contact", (req, res) => {
+  res.render("contact", {contactContent: contactContent});
+});
 
 
 
